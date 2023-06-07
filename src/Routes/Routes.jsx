1c49901edfae201e6698a443a277c8541e57e5d2
unknown <shuvrodevmondal337@gmail.com>
2023-06-07 22:50:33 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../pages/Layout/Main";
+import HomePage from "../pages/Home/HomePage/HomePage";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,11 @@
+
+const Signup = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>This Is Signup</h2>
+            
+        </div>
+    );
+};
+
+export default Signup;

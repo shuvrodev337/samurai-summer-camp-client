356d3dc187d639cd4d3704dec5489fac961e1bb2
unknown <shuvrodevmondal337@gmail.com>
2023-06-09 17:24:19 +0600

    added button disability or toggle  functionality

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { FaTrash, FaUser, FaUserNinja, FaUserShield } from "react-icons/fa";
 import axios from "axios";
-import Swal from "sweetalert2";
+
+import UsersRow from "./UsersRow";
 
 const AllUsers = () => {
 
-    // Getting All Users 
-    // const [axiosSecure] = useAxiosSecure()
+
+    // TODO Make a hook that load all users, use axios secure there.
     const {data:users=[],refetch} = useQuery(['users'],async()=>{
 
         const res  = await axios.get('http://localhost:3000/users')
@@ -14,40 +14,9 @@ const AllUsers = () => {
     })
     console.log(users);
 
-// Making A User instructor
-const makeInstructor = user =>{
-
-    Swal.fire({
-        title: 'Are you sure?',
-        text: `You want to make ${user.name} an instructor?`,
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, Do It!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-
-            fetch(`http://localhost:3000/users/admin/${user._id}`,{
-                method: 'PATCH'
-            })
-            .then(res=>res.json())
-            .then(data => {
-                if (data.modifiedCount > 0) {
-                    refetch()
-                    Swal.fire(
-                        `${user.name} has been made Instructor`,
-                        'success'
-                      )  
-                }
-            })
 
 
 
-          
-        }
-      })
-}
 
     return (
         <div className="overflow-x-auto">
@@ -65,21 +34,7 @@ const makeInstructor = user =>{
           <tbody>
             {/* row 1 */}
             {
-              users.map((user,index)=> <tr key={user._id}>
-                  <th>{index + 1}</th>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.role ==='admin'?'Admin':(user.role === 'instructor'?'Instructor':'Student')}
-                  
-                  {/* <button onClick={()=>makeAdmin(user)} className="btn btn-ghost text-xl"><FaUserShield></FaUserShield></button> */}
-              </td>
-              <td className="flex flex-col items-end gap-2 justify-center">
-                <button className="btn btn-xs btn-error"><FaTrash></FaTrash>Delete User</button>
-                <button onClick={()=>makeInstructor(user)} className="btn btn-xs btn-success "><FaUserNinja></FaUserNinja>Make Instructor</button>
-                <button className="btn btn-xs btn-warning "><FaUserShield></FaUserShield>Make Admin</button> 
-              
-              </td>
-                </tr>)
+              users.map((user,index)=> <UsersRow key={user._id} user={user} index={index} refetch={refetch}></UsersRow>)
             }
             
             
@@ -91,6 +46,3 @@ const makeInstructor = user =>{
 
 export default AllUsers;
 
-// {user.role ==='admin' && <button  className="btn btn-ghost text-xl"><FaUserShield></FaUserShield></button> }
-//                   {user.role === 'instructor' && <button  className="btn btn-ghost text-xl"><FaUserNinja></FaUserNinja></button> }
-//                   {user.role === 'student' &&<button  className="btn btn-ghost text-xl"><FaUser></FaUser></button> }
---
+++ src/pages/Dashboard/UsersRow.jsx
@@ -0,0 +1,110 @@
+import {  FaUser, FaUserNinja, FaUserShield } from "react-icons/fa";
+// import axios from "axios";
+import Swal from "sweetalert2";
+import { useState } from "react";
+const UsersRow = ({user, index,refetch}) => {
+    const [disabledInstructorBtn,setDisabledInstructorBtn] = useState(false)
+    const [disabledAdminBtn,setDisabledAdminBtn] = useState(false)
+
+
+
+// Making Any User instructor
+const makeInstructor = user =>{
+
+    if (user.role === 'instructor') {
+        return;
+      }
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: `You want to make ${user.name} an Instructor?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Do It!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            fetch(`http://localhost:3000/users/instructor/${user._id}`,{
+                method: 'PATCH'
+            })
+            .then(res=>res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    setDisabledInstructorBtn(true)
+                    setDisabledAdminBtn(false)
+                    // setDisable(true)
+                    refetch()
+
+                    Swal.fire(
+                        `${user.name} has been made Instructor`,
+                        'success'
+                      )  
+                }
+            })
+
+
+
+          
+        }
+      })
+}
+// Making Any User instructor
+const makeAdmin = user =>{
+    if (user.role === 'admin') {
+        return;
+      }
+    Swal.fire({
+        title: 'Are you sure?',
+        text: `You want to make ${user.name} an Admin?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Do It!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            fetch(`http://localhost:3000/users/admin/${user._id}`,{
+                method: 'PATCH'
+            })
+            .then(res=>res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    setDisabledAdminBtn(true)
+                    setDisabledInstructorBtn(false)
+                    // setDisable(true)
+                    refetch()
+                    Swal.fire(
+                        `${user.name} has been made Admin`,
+                        'success'
+                      )  
+                }
+            })
+
+
+
+          
+        }
+      })
+}
+
+    return (
+        <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role ==='admin'?'Admin':(user.role === 'instructor'?'Instructor':'Student')}
+                  
+              </td>
+              <td className="flex flex-col items-end gap-2 justify-center">
+                <button disabled={disabledAdminBtn || user.role === 'admin'} onClick={()=>makeAdmin(user)} className="btn btn-xs btn-warning"><FaUserShield></FaUserShield>Make Admin</button> 
+                <button disabled={disabledInstructorBtn || user.role === 'instructor'} onClick={()=>makeInstructor(user)} className="btn btn-xs btn-success"><FaUserNinja></FaUserNinja>Make Instructor</button>
+              
+              </td>
+                </tr>
+    );
+};
+
+export default UsersRow;

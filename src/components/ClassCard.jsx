54d4a44d8444ab69d2462374ca0950c505deb15d
unknown <shuvrodevmondal337@gmail.com>
2023-06-10 18:02:41 +0600

    working on feedback functionality

--- src/components/ClassCard.jsx
+++ src/components/ClassCard.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
 const ClassCard = ({singleClass,refetch}) => {
     const isAdmin = true
     const {className,instructorEmail,classPhoto,instructorName,availableSeats,price,instructorId,status,enrolledStudents} = singleClass
+    const { register,reset, handleSubmit, formState: { errors } } = useForm();
 
-    // Note: By default, the status will be pending. When an admin clicks the Approve button, the status will be updated to approved. The Deny and the Approve button will become disabled.
+      const [isOpen, setIsOpen] = useState(false);
 
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
     //  Approve , deny Functionality
 
@@ -100,6 +109,15 @@ const denyClass = classToBeDenied =>{
       })
 }
 
+// Send FeedBack
+// const sendFeedBack =(singleClass)=>{
+
+// }
+const onSubmit = (feedback)=>{
+console.log(feedback);
+console.log(className);
+closeModal()
+}
 
 
     return (
@@ -144,10 +162,48 @@ const denyClass = classToBeDenied =>{
             <div className="flex flex-col md:flex md:flex-row gap-2">
             <button disabled={disable || singleClass.status !== 'pending'} onClick={()=>approveClass(singleClass)} className="btn btn-sm btn-success">Approve</button>
             <button disabled={disable || singleClass.status !== 'pending'} onClick={()=>denyClass(singleClass)} className="btn btn-sm btn-warning">Deny</button>
-            <button className="btn btn-sm btn-info">Send Feedback</button>
+            {/* <button onClick={()=>sendFeedBack(singleClass)} className="btn btn-sm btn-info">Send Feedback</button> */}
+
             </div>
             </>
         }
+        <div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={openModal}
+      >
+        Send Feedback
+      </button>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-black opacity-75"></div>
+          <div className="bg-white p-8 w-1/2 mx-auto rounded-lg z-50">
+            <h2 className="text-xl font-semibold mb-4">Send Feedback to Instructor</h2>
+            {/* <p>Modal body text goes here...</p> */}
+            <form className="form-control" onSubmit={handleSubmit(onSubmit)}>
+            
+          <textarea className="textarea textarea-info" placeholder="Feedback"></textarea>
+          <div className="flex gap-2 justify-end">
+       
+            
+       <button
+       type="button"
+         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+         onClick={closeModal}
+       >
+         Cancel
+       </button>
+       <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4" value="Send" />
+
+          </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+        
       </div>
     </div>
     
